091dd610492e3d3e4cb9a9fabd98acbe8765930b
Nikolaos Satravelas <themrluigi@yahoo.gr>
2018-01-11 16:11:21 +0200

    Add project files.

---
+++ Co-Partnership/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Co_Partnership.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
