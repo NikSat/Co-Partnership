82c85f23f2dd87562545282be8542c4cffa7b845
vivi <vivi.panagiotidou@gmail.com>
2018-01-17 14:11:59 +0200

    StartUp: Require Https & Identity options, AccountControler: Enable lockout, Products Controller: if categoryProducts doenst have any

--- Co-Partnership/Startup.cs
+++ Co-Partnership/Startup.cs
@@ -12,6 +12,9 @@ using Co_Partnership.Data;
 using Co_Partnership.Models;
 using Co_Partnership.Services;
 using Co_Partnership.Models.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Logging;
 
 namespace Co_Partnership
 {
@@ -27,11 +30,26 @@ namespace Co_Partnership
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new RequireHttpsAttribute());
+            });
+
+            services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
 
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+            services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+                {
+                    // Password settings
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequiredUniqueChars = 2;
+                    // Signin settings
+                    options.SignIn.RequireConfirmedEmail = true;
+                    // User settings
+                    options.User.RequireUniqueEmail = true;
+                })
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
@@ -48,8 +66,16 @@ namespace Co_Partnership
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            var options = new RewriteOptions()
+               .AddRedirectToHttps();
+
+            app.UseRewriter(options);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
