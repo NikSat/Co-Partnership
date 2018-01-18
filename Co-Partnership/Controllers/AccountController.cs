48fbc7979fbfa3e2f318a712b7c500fe7d9b7c56
vivi <vivi.panagiotidou@gmail.com>
2018-01-18 12:35:34 +0200

    Email Configuration_ Need to manage user secrets to work..

--- Co-Partnership/Controllers/AccountController.cs
+++ Co-Partnership/Controllers/AccountController.cs
@@ -239,8 +239,7 @@ namespace Co_Partnership.Controllers
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     var callbackUrl = Url.EmailConfirmationLink(user.Id, code, Request.Scheme);
                     await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
-
-                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    
                     _logger.LogInformation("User created a new account with password.");
                     return RedirectToLocal(returnUrl);
                 }
