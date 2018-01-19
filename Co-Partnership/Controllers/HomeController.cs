952bc7787fa373af3187cd12b44360eca4ebec0f
Hektor <e.lentidakis@gmail.com>
2018-01-19 16:13:31 +0200

    navbar other pages fix Product display fix

--- Co-Partnership/Controllers/HomeController.cs
+++ Co-Partnership/Controllers/HomeController.cs
@@ -5,14 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Co_Partnership.Models;
+using Co_Partnership.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Co_Partnership.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly CoPartnershipContext _context;
+        public HomeController(CoPartnershipContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<ViewResult>  Index()
+        {
+            return View(await _context.Item.Where(p => (p.IsLive ?? false)).ToListAsync());
         }
 
         public IActionResult About()
