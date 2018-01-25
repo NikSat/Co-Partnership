98e3dc9291cdbf696fe275395bd2a3523bca1328
Nikolaos Satravelas <themrluigi@yahoo.gr>
2018-01-25 13:01:02 +0200

    Minor Changes

--- Co-Partnership/Controllers/AccountController.cs
+++ Co-Partnership/Controllers/AccountController.cs
@@ -64,6 +64,8 @@ namespace Co_Partnership.Controllers
             {
                 // Require the user to have a confirmed email before they can log on.
                 var user = await _userManager.FindByEmailAsync(model.Email);
+
+
                 if (user != null)
                 {
                     if (!await _userManager.IsEmailConfirmedAsync(user))
@@ -74,7 +76,9 @@ namespace Co_Partnership.Controllers
                 }
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, lockoutOnFailure: true);
+
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
