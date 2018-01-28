5b785abaceedd4d41ecf2aaac598c4a0a231b976
Nikolaos Satravelas <themrluigi@yahoo.gr>
2018-01-28 17:17:58 +0200

    Buttons

---
+++ Co-Partnership/Services/IFinanceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Co_Partnership.Services
+{
+    public class FinanceRepository
+    {
+        private ITransactionRepository transactionRepository;
+        private IUserRepository userRepository;
+        private IItemRepository itemRepository;
+
+
+    }
+}
---
+++ Co-Partnership/Services/ITransactionItemRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Co_Partnership.Models.Database;
+
+namespace Co_Partnership.Services
+{
+    interface ITransactionItemRepository
+    {
+        IQueryable<TransactionItem> TIRepository { get; }
+
+        void UpdateItem(TransactionItem item);
+
+        void SaveItem(TransactionItem item);
+
+        TransactionItem DeleteItem(int itemId);
+
+    }
+}
