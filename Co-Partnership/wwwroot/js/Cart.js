1fe85ab95a9ab324b12276cee79a4741fb27b52b
vivi <vivi.panagiotidou@gmail.com>
2018-01-28 21:41:59 +0200

    Trying Cart API

--- Co-Partnership/wwwroot/js/Cart.js
+++ Co-Partnership/wwwroot/js/Cart.js
@@ -10,18 +10,17 @@
         let row = document.createElement('tr');
         let titles = ["Product ", "Unit Price", "Quantity", "Total Price"];
 
-
-
         for (i = 0; i < titles.length; i++) {
             let cell = document.createElement('th');
             cell.classList.add("col-3");
             cell.innerHTML = titles[i];
             row.appendChild(cell);
         }
+
         head.appendChild(row);
 
         tbl.appendChild(head);
-        let targ = document.getElementById("cartItem");
+        let targ = document.getElementById("cart");
         targ.appendChild(tbl);
         let tbody = document.createElement('tbody');
         tbody.id = "CartBody";
@@ -47,9 +46,9 @@
             method: "GET",
             success: (data) => {
                 if (data.length === 0) {
-                    let targ = document.getElementById("cartItem");
+                    let targ = document.getElementById("cart");
                     let al = document.createElement('span');
-                    al.innerHTML = "Your Cart is empty..";
+                    al.innerHTML = "The Cart is empty..";
                     targ.appendChild(al);
                 }
                 else {
@@ -59,3 +58,6 @@
             }
         });
     };
+
+    getCartItems();
+});
