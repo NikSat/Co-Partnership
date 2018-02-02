8b68948d05b5b45e3f8cd76ac2c34ca58aa55cab
Hektor <e.lentidakis@gmail.com>
2018-02-02 14:45:45 +0200

    Top products home page

--- Co-Partnership/Services/ItemRepository.cs
+++ Co-Partnership/Services/ItemRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Co_Partnership.Models.Database;
 using Co_Partnership.Data;
-
+using Co_Partnership.Models;
 namespace Co_Partnership.Services
 {
     public class ItemRepository : IItemRepository
@@ -20,8 +20,28 @@ namespace Co_Partnership.Services
 
         // Get a queryable for items get all, apply queries at the controller level
         public IQueryable<Item> Items => db.Item;
+        public IQueryable<Transaction> Transactions => db.Transaction;
+        public IQueryable<TransactionItem> TransactionItems => db.TransactionItem;
 
-       
+        public IQueryable<TopProductsModel> GetTop()
+        {
+            var topProducts = (from trI in TransactionItems
+                              from tr in Transactions
+                               from it in Items
+                               where tr.IsProcessed==1
+                               where trI.Id == tr.Id
+                               where it.Id == trI.ItemId
+                               group tr by it into productGroups
+                              select new TopProductsModel
+                              {
+                                  Product =productGroups.Key,
+                                  NumberOfOrders = productGroups.Count()
+                              }).OrderByDescending(x => x.NumberOfOrders).Distinct().Take(4);
+            return topProducts;
+            //).OrderByDescending(x => x.numberOfOrders).Distinct().Take(10);
+            //    return Transactions.OrderByDescending(x => x.).Take(3)
+        }
+
         public Item DeleteItem(int itemId)
         {
             Item ite = db.Item.FirstOrDefault(p => p.Id == itemId);
