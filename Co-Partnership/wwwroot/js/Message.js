a968a7ca17e8d1ab23913036f47937d7648c243a
Nikolaos Satravelas <themrluigi@yahoo.gr>
2018-02-04 22:20:43 +0200

    JavasciptProblem

--- Co-Partnership/wwwroot/js/Message.js
+++ Co-Partnership/wwwroot/js/Message.js
@@ -1,5 +1,7 @@
 ﻿$(document).ready(function () {
 
+
+
     // AJAX CALLS HERE
     // Get all the messages
     let GetAll = () => {
@@ -40,14 +42,39 @@
 
 
 
-    // This function makes a test table 
-    let CreateTestTable =()=>{
-    
-    }
+
+    // CONSTRUCTION FUNCTIONS HERE
+    // This function creates the message table with all the messages
+    let MakeMessageListBody = () => {
+        $("#messageBody").append("<h2>Hello</h2>");
+            /*
+            `<div class="EmailHead">
+                    <h2>Inbox</h2>
+            </div>
+            <div class="subBody">
+                <div class="bodyHead">
+                    <div class="checkAll btn-small">
+                            <input type="checkbox" name="SelectAll" value="All"><a>All</a>   
+                    </div>
+                        <a id="markRead" class="btn-small">Mark Read</a>
+                        <a id="delete" class="btn-small">Delete</a>
+                </div>
+                <div class="mailList">
+            </div>`
+        );
+        */
+    };
+
+
+    let MakeMessageList = () => {
+
+
+    };
+ 
 
 
 
-
+    MakeMessageListBody();
 
 
 });
